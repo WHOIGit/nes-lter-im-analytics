de03f5654bccdda3a58e65e8d31cdad8324a0a85
Joe Futrelle <joefutrelle@gmail.com>
2022-07-01 10:31:20 -0400

    added comments, squashed timezone warning

--- plot_cumulative_timeseries.R
+++ plot_cumulative_timeseries.R
@@ -1,13 +1,19 @@
 library(tidyverse)
 library(here)
 
+# Plot cumulative uploads to EDI for the NES-LTER project
+
 csv_path <- here('knb-lter-nes_report_20220701.csv')
+
+# This csv output is from the EDI PASTA Upload Report
+# https://dashboard.edirepository.org/dashboard/reports/upload_report
 
 df <- read.csv(csv_path)
 
 df$Upload.Date.Time <- parse_datetime(df$Upload.Date.Time)
 
-p <- ggplot(df, aes(x=Upload.Date.Time, y=Count, xmin=as.POSIXct("2017-09-01", "%Y-%m-%d"))) +
+p <- ggplot(df, aes(x=Upload.Date.Time, y=Count,
+                    xmin=as.POSIXct("2017-09-01", "%Y-%m-%d", tz="UTC"))) +
   geom_point() +
   geom_line() +
   xlab("") +
